e32ef31968c29e76805bbefb7ce820c374d3c8bf
Маковеев Илья <66624012+IlyaMakoveev@users.noreply.github.com>
2025-02-17 16:57:56 +0300

    17.02 Работает экран (в дальнейшем сделать единицы измерения) Рабоатет смена режимов с ручного и автоматического)

--- sketch_jan13b.ino
+++ sketch_jan13b.ino
@@ -1,10 +1,10 @@
 #include "Wire.h"
 #include "LiquidCrystal_I2C.h"
 #include "DHT.h"
-#include <Ethernet.h>
 
-
-
+int buttonState = 0; 
+int lastButtonState = 0;
+int clickCount = 0;
 
 LiquidCrystal_I2C LCD(0x3F,16,2);
 DHT dht(2, DHT11);
@@ -13,49 +13,51 @@ void setup() {
    LCD.init();
    LCD.backlight();
    dht.begin();
-     pinMode(A1, INPUT);
-        Serial.begin(9600);
-     pinMode(3, OUTPUT);
-     pinMode(4, INPUT);
+   pinMode(A1, INPUT);
+   Serial.begin(9600);
+   pinMode(3, OUTPUT);
+   pinMode(4, INPUT_PULLUP);
 
+  int buttonState = 0;
+  int lastButtonState = 0;
+  int clickCount = 0;
 }
 
 void loop() {
-   float h = dht.readHumidity();
-   float t = dht.readTemperature();
-   int p = analogRead(A0);
-   int l = analogRead(A2);
-   bool button = digitalRead(4);
-   static bool buttonp = 0;
+   int humidity = dht.readHumidity();
+   int temp = dht.readTemperature();
+   int potenciom = analogRead(A0);
+   int light = analogRead(A2); 
+   int dl = map(light, 315, 700, 0, 255);
+   int b = map(potenciom, 0, 1023, 0, 255);
 
-   int dl = map(l, 315, 700, 0, 255);
-   int b = map(p, 0, 1023, 0, 255);
+   int clickCountPrev = clickCount;
+   bool sost = 0;
 
-   if (button == HIGH && !buttonp) {
-      buttonp = !buttonp;
-      delay(50); 
-   }
 
-   if (buttonp) {
-      analogWrite(3, b);
-      Serial.println("B");
-   } else {
-      analogWrite(3, dl);
-      Serial.println("A");
-   }
+  buttonState = digitalRead(4);
+    if (buttonState == LOW && lastButtonState == HIGH) {
+      clickCount++;
+      Serial.print("Клики: ");
+      Serial.println(clickCount);
+    }
+    lastButtonState = buttonState; 
+  // конец счетчика кноки
+  
+  if (clickCount % 2 == 0){
+    analogWrite(3, b);
+  }
 
-   LCD.setCursor(0, 0);
-   LCD.print("Humidity: ");
-   LCD.print(h);
+  if (clickCount % 2 == 1){
+    analogWrite(3, dl);
+  }
 
-   LCD.setCursor(0, 1);
-   LCD.print("Temp: ");
-   LCD.print(t);
+  LCD.setCursor(0, 0);
+  LCD.print("Humidity: ");
+  LCD.print(humidity);
 
-   int sol = analogRead(A1);
-   Serial.print("sol = ");
-   Serial.println(sol);
+  LCD.setCursor(0, 1);
+  LCD.print("Temp: ");
+  LCD.print(temp);
 
-   delay(30);
-   LCD.clear();
 }
